6ee2f59ad4914e14c7e72015cd1ea8cb68b7e3f9
RuRuRu <rururutan+github@gmail.com>
2017-10-25 20:28:53 +0900

    Enable msys's symlink.

--- install.cmd
+++ install.cmd
@@ -4,3 +4,4 @@ mklink %HOMEDRIVE%%HOMEPATH%\.emacs %PWD%windows\.emacs
 mklink %HOMEDRIVE%%HOMEPATH%\.nyagos %PWD%windows\.nyagos
 mklink %HOMEDRIVE%%HOMEPATH%\.ckw %PWD%windows\.ckw
 mklink %HOMEDRIVE%%HOMEPATH%\.xyzzy %PWD%windows\.xyzzy
+setx MSYS winsymlinks:nativestrict
